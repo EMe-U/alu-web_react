118c3e5595db17cbaa1c56b67767e144ecb2e6c5
EMe-U <e.umurerwa@alustudent.com>
2026-05-11 22:08:32 +0200

    feat: add webpack task 3 with modules and dev server

---
+++ Webpack/task_3/modules/header/header.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import './header.css';
+
+console.log('Init header');
+
+const body = $('body');
+
+body.append('<div id="logo"></div>');
+body.append('<h1>Holberton Dashboard</h1>');
